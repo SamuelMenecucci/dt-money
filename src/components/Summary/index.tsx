c08cf1d5207b830943f30bde8442a1cabbdc03f0
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-10-06 18:01:27 -0300

    dashboard and summary with styles

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,34 @@
+import incomeImg from "../../assets/entradas.svg";
+import outcomeImg from "../../assets/saídas.svg";
+import totalImg from "../../assets/total.svg";
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saídas" />
+        </header>
+        <strong> - R$500,00</strong>
+      </div>
+
+      <div className="highlight-background">
+        <header>
+          <p>Entradas</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  );
+}
---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-top: -10rem;
+
+  div {
+    background: var(--shape);
+    padding: 1.5rem 2rem;
+    border-radius: 0.25rem;
+    color: var(--text-title);
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    strong {
+      display: block; // por padrão, o strong vem com display inline, dessa forma o margin-top não funciona. O display block faz o strong se comportar como uma div e aceitar a estilização.
+      margin-top: 1rem;
+      font-size: 2rem;
+      font-weight: 500;
+      line-height: 3rem;
+    }
+
+    &.highlight-background {
+      background: var(--green);
+      color: white;
+    }
+  }
+`;
