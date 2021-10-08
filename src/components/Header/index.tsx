8540d2449b740cdba9ca7fa61c44103d0046685d
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-10-08 15:27:55 -0300

    created modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,35 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
+import { useState } from "react";
+import Modal from "react-modal";
+
 export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={handleOpenNewTransactionModal}>
+          Nova transação
+        </button>
+
+        <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h1>Cadastrar nova transação</h1>
+        </Modal>
       </Content>
     </Container>
   );
