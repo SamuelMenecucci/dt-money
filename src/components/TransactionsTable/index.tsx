6b4f59fbc4df5bb1cdd1de51884f9344eccf17dc
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-10-08 11:46:57 -0300

    install and inital configuration axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,10 @@
 import { useEffect } from "react";
 import { Container } from "./styles";
+import { api } from "../../services/api";
 
 export function TransactionsTable() {
   useEffect(() => {
-    fetch("http://localhost:3000/api/transactions")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    api.get("transactions").then((response) => console.log(response.data));
   }, []);
 
   return (
