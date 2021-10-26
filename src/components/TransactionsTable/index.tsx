a2c3d1edf5a1a4bf4743db819912af22965521cc
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-10-26 10:39:50 -0300

    formatting dates and values

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -37,9 +37,19 @@ export function TransactionsTable() {
           {transactions.map((transaction) => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>{transaction.amount}</td>
+              <td className={transaction.type}>
+                {/* utilizando uma api  nativa do browser chamada Intl para fazer a formatação de valores. utilizo o NumberFormat e passo primeiro o idioma, após isso passo o style, que é o tipo de formatação (no caso é currency pois é moeda) e após eu passo o currency para dizer qual moeda. após, coloco o format ára dizer o que eu vou formatar.   */}
+                {new Intl.NumberFormat("pt-br", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.amount)}
+              </td>
               <td>{transaction.category}</td>
-              <td>{transaction.createdAt}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-br").format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
